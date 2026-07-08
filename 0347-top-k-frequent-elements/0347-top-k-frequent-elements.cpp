cb4012c26b46d360a5882933cbe4f5024271036e
Shubham Tidke <tidkeshubham826@gmail.com>
2026-07-08 18:15:10 +0530

    Time: 3 ms (50.45%), Space: 18.4 MB (27.57%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>ump; 
+        for(int i=0;i<nums.size();i++){
+            if(ump.find(nums[i])==ump.end()){
+                ump[nums[i]]=1;
+            }else{
+                ump[nums[i]]=ump[nums[i]]+1;
+            }
+        }
+
+        vector<int>ans(k,0);
+        multimap<int,int>mp;
+        int ind=0;
+        for(pair<int,int> val:ump){
+            //cout<<val.first<<" "<<val.second<<endl;    //sorted order
+            mp.insert({val.second,val.first});
+            ind++;
+        }
+
+        int curr=0;
+        int i=0;
+        for(pair<int,int> val:mp){
+            if(ind-k<=curr){
+                ans[i]=val.second;
+                i++;
+            }curr++;
+        }
+        return ans;
+
+    }
+};
